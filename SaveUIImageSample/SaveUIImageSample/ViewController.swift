c41d0f9b53d46888b11fabc8e2ea030c77b29cc1
Yuki Sumida <y.sumida.0201@gmail.com>
2017-11-11 23:04:29 +0900

    カメラロールから写真を選択して表示する

---
+++ SaveUIImageSample/SaveUIImageSample/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  SaveUIImageSample
+//
+//  Created by Yuki Sumida on 2017/11/11.
+//  Copyright © 2017年 Yuki Sumida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UINavigationControllerDelegate {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func tapButton(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = .photoLibrary
+            self.present(picker, animated: true, completion: nil)
+        }
+    }
+}
+
+extension ViewController: UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        self.imageView.image = image
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
